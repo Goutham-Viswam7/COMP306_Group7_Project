cb9bcaa51c0b50099ab3255c0b3825072ee21450
Goutham Viswam <g199@my.centennialcollege.ca>
2020-11-27 16:09:41 -0500

    Hospital App added

---
+++ HospitalApp/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HospitalApp.Models;
+using HospitalApp.Helper;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace HospitalApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        DonorAPI _api = new DonorAPI();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            List<DonorsData> donors = new List<DonorsData>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/donors");
+            if(res.IsSuccessStatusCode)
+            {
+                var results = res.Content.ReadAsStringAsync().Result;
+                donors = JsonConvert.DeserializeObject<List<DonorsData>>(results);
+            }
+            return View(donors);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            DonorsData donors = new DonorsData();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync($"api/donors/{id}");
+            if (res.IsSuccessStatusCode)
+            {
+                var results = res.Content.ReadAsStringAsync().Result;
+                donors = JsonConvert.DeserializeObject<DonorsData>(results);
+            }
+            return View(donors);
+        }
+
+        [HttpPost]
+        public IActionResult create(DonorsData donor)
+        {
+            HttpClient client = _api.Initial();
+
+            var postTask = client.PostAsJsonAsync<DonorsData>("api/donors", donor);
+            postTask.Wait();
+
+            var result = postTask.Result;
+            if(result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            DonorsData donors = new DonorsData();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.DeleteAsync($"api/donors/{id}");
+          
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, DonorsData donor)
+        {
+            HttpClient client = _api.Initial();
+            var postTask = client.PutAsJsonAsync<DonorsData>($"api/donors/{id}",donor);
+            postTask.Wait();
+
+            var result = postTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            DonorsData donors = new DonorsData();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync($"api/donors/{id}");
+            if (res.IsSuccessStatusCode)
+            {
+                var results = res.Content.ReadAsStringAsync().Result;
+                donors = JsonConvert.DeserializeObject<DonorsData>(results);
+            }
+            return View(donors);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HospitalApp/Models/DonorsData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalApp.Models
+{
+    public class DonorsData
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public string BloodGroup { get; set; }
+
+        public string Address { get; set; }
+    }
+}
